19664c11f1fa358677c447e7f5451c44fc6e92b9
OR6107 <191210ogr@kaiseigakuen.jp>
2020-12-31 07:10:06 +0900

    Update script.js

--- puzzle/events/advent2020/style/dist/script.js
+++ puzzle/events/advent2020/style/dist/script.js
@@ -1,4 +1,4 @@
-const dataset = [5,2,2,1,1,1,10];
+const dataset = [5,2,2,1,1,1];
 
 // let colors = ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd'];
 // let colors = ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#e0e0e0', '#bababa', '#878787', '#4d4d4d', '#1a1a1a'];
